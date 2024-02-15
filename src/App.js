a4748243f714ead7adef0c2cddd1d20f34762935
송성빈 <killpig00@naver.com>
2024-02-15 16:45:53 +0900

    temp

--- src/App.js
+++ src/App.js
@@ -21,39 +21,66 @@ function App() {
     
       <div className="skills">
         <div className="skills-container">
-            <h1>Skills</h1>
+            <h1 style={{color:"#f4623a"}}>Skills</h1>
               <div className="skills-frontend">
                 
                 <div className="fronted-boxes">
-                  <p style={{color:"orange"}}>많이 사용해봤어요</p>
-                  <p>HTML</p>
-                  <p>CSS</p>
-                  <p>Java Script</p>
+                  
+                  <p style={{marginTop:"60px"}}>
+                    <img src="/images/front1.png" className="skills-img1" ></img>
+                  </p>
+               
                 </div>
               
                 <div className="fronted-boxes">
-                    <p style={{color:"orange"}}>자주 써봤어요</p>
-                    <p>React</p>
-                    <p>NEXT.js</p>
-                    <p>React Query</p>
-                    <p>React-toolkit</p>
-                    <p>Boot strap</p>
+                    <h3 style={{color:"#f4623a",marginBottom:"0px",marginTop:"5px"}}>Frontend</h3>
+                    <hr></hr>
+                    <p><img src="/images/front2.png" className="skills-img2" ></img></p>
+               
                 </div>
                 
                 <div className="fronted-boxes">
-                    <p style={{color:"orange"}}>사용 경험이 있어요</p>
-                    <p>Jquery</p>
-                    <p>SCSS</p>
+                    
+                    <p style={{marginTop:"60px"}}>
+                    <img src="/images/front3.png" className="skills-img3" ></img>
+                    </p>
+               
     
                 </div>
               </div>
-
-            <div style={{display:"flex",width:"70%",height:"100%"}}>
-              <div className="skills-backend">
-                csd
+            <div className="skills-backend-container" style={{display:"flex", margin:"auto",maxWidth:"1050px"}}>
+              <div className="skills-backend skills-backend-bone">
+                <div style={{width:"50%",margin:"auto"}}>
+                <h3 style={{color:"#f4623a",marginBottom:"10px",marginTop:"15px"}}>Backend</h3>
+                <hr></hr>
+                </div>
+                <p><img src="/images/backend.png" className="skills-img5" ></img></p>
               </div>
-              <div className="skills-version">
-                asdasas
+              <div className="skills-version skills-backend-bone">
+                <div style={{width:"50%",margin:"auto"}}>
+                <h3 style={{color:"#f4623a",marginBottom:"10px",marginTop:"15px"}}>Version-control</h3>
+                <hr></hr>
+                </div>
+              <p><img src="/images/front5.png" className="skills-img4" ></img></p>
+               
+              </div>
+            </div>
+
+            <div style={{display:"flex",margin:"auto",width:"53%",height:"100%",maxWidth:"1050px"}}>
+              <div className="skills-database skills-backend-bone">
+                <div style={{width:"50%",margin:"auto"}}>
+                <h3 style={{color:"#f4623a",marginBottom:"10px",marginTop:"15px"}}>Database</h3>
+                <hr></hr>
+                </div>
+                <p><img src="/images/database.png" className="skills-img6" ></img></p>
+              </div>
+              <div className="skills-android skills-backend-bone">
+                <div style={{width:"50%",margin:"auto"}}>
+                <h3 style={{color:"#f4623a",marginBottom:"10px",marginTop:"15px"}}>Android</h3>
+                <hr></hr>
+                </div>
+              <p><img src="/images/android.png" className="skills-img7" ></img></p>
+               
               </div>
             </div>
 
